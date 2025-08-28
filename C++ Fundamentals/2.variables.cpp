889162498862175874c88408ef3ed91b78076f65
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-08-28 22:30:50 +0530

    update

---
+++ C++ Fundamentals/2.variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num=3;
+
+    int x;
+    x=4;
+
+    cout<<num<<endl;
+
+    num=7;
+
+    cout<<x<<endl;
+
+    cout<<num<<endl;
+}
