7d8f3a02077937d6438595dbab526382539c226f
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-09-10 17:43:28 +0530

    update

---
+++ DSA/1.Pattern printing/Basic level problems/6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row;
+    char col;
+    for(row=1; row<=5; row++){
+        for(col='a'; col<='e'; col++){
+            
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}
---
+++ DSA/1.Pattern printing/Medium level problems/5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int row, col;
+    for(row=1; row<=5; row++){
+        char name='a'+row-1;
+        for(col=1; col<=row; col++){
+            cout<<name<<" ";
+
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
